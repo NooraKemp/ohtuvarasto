967511e2d0d946c18fc433a7cc8316e887983d5c
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2022-11-11 10:03:46 +0200

    update week 2 task 3

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,12 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+        self.tilavuus=max(0.0, tilavuus)
+        self.saldo=max(0.0, alku_saldo)
+        self.saldo=min(self.tilavuus, self.saldo)
+
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kentää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -31,7 +21,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +30,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
